2f8bf32feb33c60a4028b733e2b56ac19ffe47e5
Juan Dausa <juandausa@gmail.com>
2019-09-01 23:29:28 -0300

    Se agrega un proyecto de test de ejemplo

---
+++ Test.Base/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Base
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Test.Base/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Base")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test.Base")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0cd98d7c-0f8b-4165-8ac9-3b2a734cdbf6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
